969034bf0fffbc643a10382f9b6415f4b14ca29b
周生 <8181872+zhou-sheng1@user.noreply.gitee.com>
2023-09-06 23:26:48 +0800

    提交

--- src/main/java/com/zys/http/ui/icon/HttpIcons.java
+++ src/main/java/com/zys/http/ui/icon/HttpIcons.java
@@ -19,12 +19,12 @@ public final class HttpIcons {
     public static final Icon REMOVE = IconLoader.getIcon("/icon/remove.svg", HttpIcons.class);
 
     // 请求方法
-    public static final Icon REQUEST = IconLoader.getIcon("/icon/method/request.svg", HttpIcons.class);
-    public static final Icon GET = IconLoader.getIcon("/icon/method/get.svg", HttpIcons.class);
-    public static final Icon POST = IconLoader.getIcon("/icon/method/post.svg", HttpIcons.class);
-    public static final Icon PUT = IconLoader.getIcon("/icon/method/put.svg", HttpIcons.class);
-    public static final Icon DELETE = IconLoader.getIcon("/icon/method/delete.svg", HttpIcons.class);
-    public static final Icon PATCH = IconLoader.getIcon("/icon/method/patch.svg", HttpIcons.class);
-    public static final Icon HEADER = IconLoader.getIcon("/icon/method/header.svg", HttpIcons.class);
-    public static final Icon OPTIONS = IconLoader.getIcon("/icon/method/options.svg", HttpIcons.class);
+    public static final Icon REQUEST = IconLoader.getIcon("/icon/http/request.svg", HttpIcons.class);
+    public static final Icon GET = IconLoader.getIcon("/icon/http/get.svg", HttpIcons.class);
+    public static final Icon POST = IconLoader.getIcon("/icon/http/post.svg", HttpIcons.class);
+    public static final Icon PUT = IconLoader.getIcon("/icon/http/put.svg", HttpIcons.class);
+    public static final Icon DELETE = IconLoader.getIcon("/icon/http/del.svg", HttpIcons.class);
+    public static final Icon PATCH = IconLoader.getIcon("/icon/http/patch.svg", HttpIcons.class);
+    public static final Icon HEADER = IconLoader.getIcon("/icon/http/header.svg", HttpIcons.class);
+    public static final Icon OPTIONS = IconLoader.getIcon("/icon/http/options.svg", HttpIcons.class);
 }
